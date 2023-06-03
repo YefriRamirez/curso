6ea41bd4c3955719fe9866c8342f434830bf4ad8
YefriRamirez <135410919+YefriRamirez@users.noreply.github.com>
2023-06-03 00:47:03 -0600

    Initial commit

---
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -0,0 +1,32 @@
+package com.cursojava.curso.models;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter @Column(name = "id")
+    private Long id;
+
+    @Getter @Setter @Column (name = "nombre")
+    private String nombre;
+
+    @Getter @Setter @Column (name = "apellido")
+    private String apellido;
+
+    @Getter @Setter @Column (name = "email")
+    private String email;
+
+    @Getter @Setter @Column (name = "telefono")
+    private String telefono;
+
+    @Getter @Setter @Column (name = "password")
+    private String password;
+
+    }
+
